cc8d8c6c80a33841c1a29045eedd6fb4ebe759df
Nicolai30 <nicolaimillanest@gmail.com>
2024-10-03 21:31:04 -0300

    Primer Commit

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import Footer from './components/layout/footer/Footer'
+// import NavBar from './components/layout/Navbar/NavBar.Jsx'
+
+function App() {
+  return (
+    <>
+      {/* <NavBar /> */}
+      <Footer />
+    </>
+  )
+}
+
+export default App
