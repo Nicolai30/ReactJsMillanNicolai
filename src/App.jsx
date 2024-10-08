da63645afd18bd80fc126ab4fe0df1fe69486910
Nicolai30 <nicolaimillanest@gmail.com>
2024-10-08 16:24:44 -0300

    Subiendo correcciones del tutor

--- src/App.jsx
+++ src/App.jsx
@@ -2,10 +2,24 @@ import NavBar from './components/layout/NavBar/NavBar'
 import { ItemListContainer } from './components/pages/ItemListContainer/ItemListContainer'
 
 function App() {
+  //Productos
+  const items = [
+    { title: "Pintura Abstracta En Blanco Y Negro", price: 1200, artist: "Steve Johnson", stock: 5, ImageSrc: "https://res.cloudinary.com/df0naogwq/image/upload/v1728353729/eqmkczrsbgn91bhyyhy1.jpg" },
+    { title: "Cuadro Abstracto Naranja Y Azul", price: 1200, artist: "Paul Blenkhorn", stock: 5, ImageSrc: "https://res.cloudinary.com/df0naogwq/image/upload/v1728353873/v2ztp3nmfuz5rislpjj6.jpg" },
+    { title: "Cuadro Abstracto Naranja Y Azul", price: 1200, artist: "Diana", stock: 5, ImageSrc: "https://res.cloudinary.com/df0naogwq/image/upload/v1728353565/qjzrehwhkygo09guy6u9.jpg" },
+    { title: "Cuadro Abstracto Naranja Y Azul", price: 1200, artist: "Paul Blenkhorn", stock: 5, ImageSrc: "https://res.cloudinary.com/df0naogwq/image/upload/v1728353875/lfte1hmpeawpvub9ee7a.jpg" },
+    { title: "Real Madre", price: 1200, artist: "zhang kaiyv", stock: 5, ImageSrc: "https://res.cloudinary.com/df0naogwq/image/upload/v1728354141/be6s0ua2lnagfumy1hbt.jpg" },
+    { title: "Repe-2", price: 1200, artist: "Paul Blenkhorn", stock: 5, ImageSrc: "https://res.cloudinary.com/df0naogwq/image/upload/v1728353873/v2ztp3nmfuz5rislpjj6.jpg" },
+    { title: "Repe-5", price: 1200, artist: "zhang kaiyv", stock: 5, ImageSrc: "https://res.cloudinary.com/df0naogwq/image/upload/v1728354141/be6s0ua2lnagfumy1hbt.jpg" },
+    { title: "Repe-4", price: 1200, artist: "Paul Blenkhorn", stock: 5, ImageSrc: "https://res.cloudinary.com/df0naogwq/image/upload/v1728353875/lfte1hmpeawpvub9ee7a.jpg" },
+    { title: "Repe-1", price: 1200, artist: "Steve Johnson", stock: 5, ImageSrc: "https://res.cloudinary.com/df0naogwq/image/upload/v1728353729/eqmkczrsbgn91bhyyhy1.jpg" },
+    { title: "Repe-3", price: 1200, artist: "Diana", stock: 5, ImageSrc: "https://res.cloudinary.com/df0naogwq/image/upload/v1728353565/qjzrehwhkygo09guy6u9.jpg" }
+  ];
+  
   return (
     <>
       <NavBar/>
-      <ItemListContainer />
+      <ItemListContainer items={items} />
     </>
   )
 }
--- src/components/pages/ItemListContainer/ItemListContainer.jsx
+++ src/components/pages/ItemListContainer/ItemListContainer.jsx
@@ -1,19 +1,20 @@
 import  "./ItemListContainer.css"
 import ItemCard from "../../common/ItemCard/ItemCard";
 
-export const ItemListContainer = () => {
+export const ItemListContainer = ({items}) => {
   return (
     <div className="ItemListContainer">
-      <ItemCard title="Pintura Abstracta En Blanco Y Negro" price={1200} artist = "Steve Johnson" stock={5} className="card" ImageSrc="https://res.cloudinary.com/df0naogwq/image/upload/v1728353729/eqmkczrsbgn91bhyyhy1.jpg"/>
-      <ItemCard title="Cuadro Abstracto Naranja Y Azul" price={1200} artist = "Paul Blenkhorn" stock={5} className="card" ImageSrc="https://res.cloudinary.com/df0naogwq/image/upload/v1728353873/v2ztp3nmfuz5rislpjj6.jpg"/>
-      <ItemCard title="Cuadro Abstracto Naranja Y Azul" price={1200} artist = "Diana" stock={5} className="card" ImageSrc="https://res.cloudinary.com/df0naogwq/image/upload/v1728353565/qjzrehwhkygo09guy6u9.jpg"/>
-      <ItemCard title="Cuadro Abstracto Naranja Y Azul" price={1200} artist = "Paul Blenkhorn" stock={5} className="card" ImageSrc="https://res.cloudinary.com/df0naogwq/image/upload/v1728353875/lfte1hmpeawpvub9ee7a.jpg"/>
-      <ItemCard title="Real Madre" price={1200} artist = "zhang kaiyv" stock={5} className="card" ImageSrc="https://res.cloudinary.com/df0naogwq/image/upload/v1728354141/be6s0ua2lnagfumy1hbt.jpg"/>
-      <ItemCard title="Repe-2" price={1200} artist = "Paul Blenkhorn" stock={5} className="card" ImageSrc="https://res.cloudinary.com/df0naogwq/image/upload/v1728353873/v2ztp3nmfuz5rislpjj6.jpg"/>
-      <ItemCard title="Repe-5" price={1200} artist = "zhang kaiyv" stock={5} className="card" ImageSrc="https://res.cloudinary.com/df0naogwq/image/upload/v1728354141/be6s0ua2lnagfumy1hbt.jpg"/>
-      <ItemCard title="Repe-4" price={1200} artist = "Paul Blenkhorn" stock={5} className="card" ImageSrc="https://res.cloudinary.com/df0naogwq/image/upload/v1728353875/lfte1hmpeawpvub9ee7a.jpg"/>
-      <ItemCard title="Repe-1" price={1200} artist = "Steve Johnson" stock={5} className="card" ImageSrc="https://res.cloudinary.com/df0naogwq/image/upload/v1728353729/eqmkczrsbgn91bhyyhy1.jpg"/>
-      <ItemCard title="Repe-3" price={1200} artist = "Diana" stock={5} className="card" ImageSrc="https://res.cloudinary.com/df0naogwq/image/upload/v1728353565/qjzrehwhkygo09guy6u9.jpg"/>
+      {items.map((item, index) => (
+        <ItemCard 
+          key={index}
+          title={item.title} 
+          price={item.price} 
+          artist={item.artist} 
+          stock={item.stock} 
+          className="card" 
+          ImageSrc={item.ImageSrc}
+        />
+      ))}
     </div>
   );
 };
